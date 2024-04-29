9c84f6a2abf51be8fa1bac4b5b45f66cbcee1a15
SunLxy <1011771396@qq.com>
2024-04-29 20:24:16 +0800

    init:初始化

---
+++ core/README.md
@@ -0,0 +1,7 @@
+# 表格
+
+基于[ali-react-table](https://github.com/alibaba/ali-react-table)进行添加功能
+
+1. 添加可进行导出表格
+2. 表头添加过滤
+3. 添加列拖拽
---
+++ examples/src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import APP from './routes';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <APP />,
+);
